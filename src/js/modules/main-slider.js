db190ba402d435ccd8656e6551c542ec56a88dcf
AngryFennec <angryfennecus@gmail.com>
2020-02-02 00:35:28 +0300

    Слайдеры только не оч

--- src/js/modules/main-slider.js
+++ src/js/modules/main-slider.js
@@ -6,5 +6,51 @@
        parallax: true,
        speed: 600,
 centeredSlides: true,
+    onlyExternal:true
    });
+
+var miniSwipers = Array.from(document.querySelectorAll('.gallery__slider .swiper-container'));
+var jsMiniSwipers = [];
+if (miniSwipers.length !== 0) {
+
+  miniSwipers.forEach(function(item, i) {
+
+    jsMiniSwipers.push(new Swiper(item, {
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'progressbar',
+    },
+    //onlyExternal:true
+  }));
+});
+
+}
 })();
+/*
+const $slider = $("#slider");
+$slider
+	.on('init', () => {
+		mouseWheel($slider)
+	})
+	.slick({
+		vertical: true,
+		infinite: false,
+	})
+function mouseWheel($slider) {
+
+	$(window).on('wheel', { $slider: $slider }, mouseWheelHandler)
+}
+function mouseWheelHandler(event) {
+	event.preventDefault();
+  console.log('wheel');
+
+	const $slider = event.data.$slider
+	const delta = event.originalEvent.deltaY
+	if(delta <= 0) {
+		$slider.slick('slickPrev')
+	}
+	else {
+		$slider.slick('slickNext')
+	}
+}
+*/
