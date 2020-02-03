14746838754e218a7b9bc73347caceb3c3096ecb
dev16 <dev16@idea-labs.ru editor = "C:\Programs\Microsoft VS Code\Code.exe" --wait>
2020-02-03 19:34:44 +0500

    правки

--- wp/functions.php
+++ wp/functions.php
@@ -1,5 +1,8 @@
 <?php
 
+// update_option('siteurl','http://logvinb9.beget.tech');
+// update_option('home','http://logvinb9.beget.tech');
+
 function include_css () {
     wp_enqueue_style( 'style', get_template_directory_uri() . '/assets/css/style.min.css');
 }
@@ -12,14 +15,15 @@ function include_script () {
 add_action( 'wp_enqueue_scripts', 'include_css' );
 add_action( 'wp_enqueue_scripts', 'include_script' );
 
-add_action( 'init', 'true_register_post_type_init' ); // Использовать функцию только внутри хука init
+add_action( 'init', 'true_register_post_type_init' );
+add_action( 'init', 'true_register_post_type_portfolio_init' ); 
  
 function true_register_post_type_init() {
 	$labels = array(
 		'name' => 'Service',
-		'singular_name' => 'Service', // админ панель Добавить->Функцию
+		'singular_name' => 'Service', 
 		'add_new' => 'add service',
-		'add_new_item' => 'add new service', // заголовок тега <title>
+		'add_new_item' => 'add new service', 
 		'edit_item' => 'edit service',
 		'new_item' => 'new service',
 		'all_items' => 'All services',
@@ -27,24 +31,55 @@ function true_register_post_type_init() {
 		'search_items' => 'search services',
 		'not_found' =>  'not found',
 		'not_found_in_trash' => 'not found',
-		'menu_name' => 'Service' // ссылка в меню в админке
+		'menu_name' => 'Service' 
 	);
 	$args = array(
 		'labels' => $labels,
 		'public' => true,
-		'show_ui' => true, // показывать интерфейс в админке
+		'show_ui' => true,
 		'has_archive' => true, 
-		// 'menu_icon' => get_stylesheet_directory_uri() .'/img/function_icon.png', // иконка в меню
-		'menu_position' => 20, // порядок в меню
+		'menu_position' => 30, 
 		'supports' => array( 'title')
 	);
 	register_post_type('functions', $args);
 }
 
+function true_register_post_type_portfolio_init() {
+	$labels = array(
+		'name' => 'Portfolio',
+		'singular_name' => 'Portfolio', 
+		'add_new' => 'add Portfolio',
+		'add_new_item' => 'add new Portfolio',
+		'edit_item' => 'edit Portfolio',
+		'new_item' => 'new Portfolio',
+		'all_items' => 'All Portfolio',
+		'view_item' => 'view Portfolio',
+		'search_items' => 'search Portfolio',
+		'not_found' =>  'not found',
+		'not_found_in_trash' => 'not found',
+		'menu_name' => 'Portfolio' 
+	);
+	$args = array(
+		'labels' => $labels,
+		'public' => true,
+		'show_ui' => true, 
+		'has_archive' => true, 
+		'menu_position' => 20, 
+		'supports' => array( 'title')
+	);
+	register_post_type('Portfolio', $args);
+}
+
 add_action('init', function() {
-    pll_register_string('mytheme-header', 'Our Portfolio');
+	pll_register_string('mytheme-header', 'Our Portfolio');
+	pll_register_string('mytheme-header', 'Main Page');
     pll_register_string('mytheme-header', 'Privacy Policy');
-    pll_register_string('mytheme-feadback', 'Let’s discuss your project');
+	pll_register_string('mytheme-feadback', 'Let’s discuss your project');
+	pll_register_string('mytheme-feadback', 'VR');
+	pll_register_string('mytheme-feadback', '3D');
+	pll_register_string('mytheme-feadback', 'IVT');
+	pll_register_string('mytheme-feadback', 'DS');
+	pll_register_string('mytheme-feadback', '360');
 });
 
 ?>
--- wp/header.php
+++ wp/header.php
@@ -4,23 +4,25 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://fonts.googleapis.com/css?family=DM+Serif+Display:400,400i&display=swap&subset=latin-ext" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=DM+Serif+Display:400,400i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&display=swap&subset=cyrillic,cyrillic-ext,latin-ext" rel="stylesheet">
   <link rel="shortcut icon" href="<?php echo  get_template_directory_uri() . '/assets/img/' ;?>favicon.png" type="image/x-icon">
-  <title>Northman</title>
+  <title>Northman VR</title>
 
 
   <?php wp_head() ?>
 </head>
 
-<body class="page-body">
+<body class="page-body <?php if ($pagename === 'our-portfolio' || $pagename === 'our-portfolio-2'||$pagename === 'our-portfolio-3' ||$pagename === 'our-portfolio-4') echo 'page-body--portfolio';?>">
   <span class="decor decor--big">Nortman VR</span>
   <h1>Northman</h1>
   <header class="header">
     <nav class="header__menu">
-      <a href="/wp/portfolio/" class="header__portfolio-link"><?php pll_e('Our Portfolio'); ?></a>
+    <?php if ($pagename === 'our-portfolio' || $pagename === 'our-portfolio-2'||$pagename === 'our-portfolio-3' ||$pagename === 'our-portfolio-4') :?>
+      <a href="/" class="header__portfolio-link"><?php pll_e('Main Page'); ?></a>
+      <?php else :?>
+        <a href="/our-portfolio/" class="header__portfolio-link"><?php pll_e('Our Portfolio'); ?></a>
+      <?php endif;?>
       <div class="header__logo-block">
-        <img class="header__logo" src="<?php echo  get_template_directory_uri() . '/assets/img/' ;?>logo.svg">
+        <a href="/"><img class="header__logo" src="<?php echo  get_template_directory_uri() . '/assets/img/' ;?>logo.svg" alt="logo"></a>
       </div>
       <ul><?php pll_the_languages([
           'display_names_as' => 'slug'
