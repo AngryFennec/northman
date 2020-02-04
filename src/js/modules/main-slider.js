d67c5a6a93e76b3e3208aaca9fd49badf9cc0443
AngryFennec <angryfennecus@gmail.com>
2020-02-04 21:45:42 +0300

    воткнула слайдер в дрона

--- src/js/modules/main-slider.js
+++ src/js/modules/main-slider.js
@@ -1,51 +1,41 @@
 (function() {
 
-  /*  var swiper = new Swiper('.gallery__main-slider', {
-         direction: 'vertical',
-         slidesPerView: 1.5,
-         mousewheelControl: true,
-         parallax: true,
-         speed: 600,
-      onlyExternal:true
-     });
-  */
+  var mainSlider = document.querySelector('.gallery__main-slider-container');
+  if(mainSlider) {
+    var mainSwiper = new Swiper (mainSlider, {
+  			// Optional parameters
+        direction: 'vertical',
+        slidesPerView: 1,
+        mousewheel: true,
+        parallax: true,
+        speed: 600,
+        centeredSlides: true,
 
+      });
 
-var mainSlider = document.querySelector('.gallery__main-slider-container');
-if(mainSlider) {
-  var mainSwiper = new Swiper (mainSlider, {
-			// Optional parameters
-      direction: 'vertical',
-      slidesPerView: 1,
-      mousewheel: true,
-      parallax: true,
-      speed: 600,
-      centeredSlides: true,
-
-    });
-
-var counter = 0;
-window.addEventListener("wheel", event =>  {
-  if(event.deltaY < 0) {
-    counter = 0;
-  }
-  if (mainSwiper.activeIndex === mainSwiper.slides.length-1 && event.deltaY > 0) {
-    if (counter > 0) {
-      window.scrollBy({
-  top: 300,
-  behavior: 'smooth'
-});
-    } else {
-      counter++;
+  var counter = 0;
+  window.addEventListener("wheel", event =>  {
+    if(event.deltaY < 0) {
+      counter = 0;
     }
-  }
-});
+    if (mainSwiper.activeIndex === mainSwiper.slides.length-1 && event.deltaY > 0) {
+      if (counter > 0) {
+        window.scrollBy({
+    top: 300,
+    behavior: 'smooth'
+  });
+      } else {
+        counter++;
+      }
+    }
+  });
 
-  }
+    }
 
   var s1 = document.querySelector('.js-gallery-first .swiper-container');
   var s2 = document.querySelector('.js-gallery-second .swiper-container');
-  if (s1 && s2) {
+  var s3 = document.querySelector('.js-gallery-third .swiper-container');
+  if (s1 && s2 && s3) {
   var firstSwiper = new Swiper(s1, {
     pagination: {
       el: '.swiper-pagination-first',
@@ -62,6 +52,13 @@ window.addEventListener("wheel", event =>  {
     //onlyExternal:true
   });
 
+  var thirdSwiper = new Swiper(s3, {
+    pagination: {
+      el: '.swiper-pagination-third',
+      type: 'progressbar',
+    },
+    //onlyExternal:true
+  });
   var spanCurrentFirst = document.querySelector('.js-gallery-first .js-current');
   firstSwiper.on('slideChange', function() {
     var num = firstSwiper.activeIndex + 1;
@@ -77,9 +74,17 @@ window.addEventListener("wheel", event =>  {
     str = num < 10 ? '0' + num : num;
     spanCurrentSecond.textContent = str;
   });
+  var spanCurrentThird = document.querySelector('.js-gallery-third .js-current');
+  thirdSwiper.on('slideChange', function() {
+    var num = thirdSwiper.activeIndex + 1;
+    var str;
+    str = num < 10 ? '0' + num : num;
+    spanCurrentThird.textContent = str;
+  });
 
   var spanAllFirst = document.querySelector('.js-gallery-first .js-all');
   var spanAllSecond = document.querySelector('.js-gallery-second .js-all');
+  var spanAllThird = document.querySelector('.js-gallery-third .js-all');
 
   var num1 = firstSwiper.slides.length;
   var str1;
@@ -89,5 +94,35 @@ window.addEventListener("wheel", event =>  {
   var str2;
   str2 = num2 < 10 ? '0' + num2 : num2;
   spanAllSecond.textContent = str2;
+  var num3 = thirdSwiper.slides.length;
+  var str3;
+  str3 = num3 < 10 ? '0' + num3 : num3;
+  spanAllThird.textContent = str3;
 }
 })();
+/*
+const $slider = $("#slider");
+$slider
+	.on('init', () => {
+		mouseWheel($slider)
+	})
+	.slick({
+		vertical: true,
+		infinite: false,
+	})
+function mouseWheel($slider) {
+	$(window).on('wheel', { $slider: $slider }, mouseWheelHandler)
+}
+function mouseWheelHandler(event) {
+	event.preventDefault();
+  console.log('wheel');
+	const $slider = event.data.$slider
+	const delta = event.originalEvent.deltaY
+	if(delta <= 0) {
+		$slider.slick('slickPrev')
+	}
+	else {
+		$slider.slick('slickNext')
+	}
+}
+*/
